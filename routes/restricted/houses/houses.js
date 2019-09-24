89d153114bc5929b6da667f55e31240f64344d6d
Arvin Agas <arvagas@gmail.com>
2019-09-24 19:16:46 -0400

    connect DS w/ backend

--- routes/restricted/houses/houses.js
+++ routes/restricted/houses/houses.js
@@ -2,22 +2,14 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 const Houses = require('./housesModel');
+const Prices = require('./pricesModel')
 const mw = require('./housesMiddleware');
 const authMiddleware = require('../../../auth/authMiddleware');
 
 
 router.get('/', authMiddleware, (req, res) => {
     Houses.find()
-    .then(houses => {
-        
-        //move this to post
-        res.status(200).json(houses)
-//axios.get string interpolate link replace values(key,value) 
-        axios.post(`https://appraisers-bff.herokuapp.com/?bedrooms=3&bathrooms=2&squarefeet=1800&zipcode=96337&yearbuilt=1979`)
-        .then()
-        .catch(err)
-        //nested get ""
-    }) 
+    .then(houses => res.status(200).json(houses)) 
     .catch(err => res.status(400).json({ err: err.message }))
 });
 
@@ -33,7 +25,12 @@ router.post('/', authMiddleware, mw.validateHouseObj, (req, res) => {
     const newHouse = req.body;
 
     Houses.add(newHouse)
-        .then(house => res.status(201).json(newHouse))
+        .then(house => {
+            axios.get(`https://appraisers-bff.herokuapp.com/?bedrooms=${house.bedrooms}&bathrooms=${house.bathrooms}&squarefeet=${house.squareFootage}&yearbuilt=${house.yearBuilt}&zipcode=11111`)
+                .then(res => Prices.add(house.id, res.data))
+                .catch(err => res.status(500).json({ message: 'error calculating price' }))
+            res.status(201).json(house)
+        })
         .catch(err => res.status(400).json({ err: err.message }))
 });
 
@@ -42,7 +39,12 @@ router.put('/:id', authMiddleware, mw.validateHouseId, mw.validateHouseObj, (req
     const changes = req.body;
 
     Houses.update(id, changes)
-        .then(house => res.status(200).json(house))
+        .then(house => {
+            axios.get(`https://appraisers-bff.herokuapp.com/?bedrooms=${house.bedrooms}&bathrooms=${house.bathrooms}&squarefeet=${house.squareFootage}&yearbuilt=${house.yearBuilt}&zipcode=11111`)
+                .then(res => Prices.update(house.id, res.data))
+                .catch(err => res.status(500).json({ message: 'error calculating price' }))
+            res.status(200).json(house)
+        })
         .catch(err => res.status(400).json({ err: err.message }))
 });
 
