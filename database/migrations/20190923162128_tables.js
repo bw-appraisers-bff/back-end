fd9cce85a00c56abed4fe8e9c6c050209ac1ce3c
Arvin Agas <arvagas@gmail.com>
2019-09-24 17:43:54 -0400

    created prices table along w/ seed data

--- database/migrations/20190923162128_tables.js
+++ database/migrations/20190923162128_tables.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable('users', tbl => {
+    .createTable('users', tbl => {
       tbl.increments();
       tbl.text('username', 256)
       // minimal length 
@@ -9,46 +9,59 @@ exports.up = function(knex) {
         .unique()
       tbl.string('password', 256).notNullable();
         // minimal length 
-  })
-  .createTable('houses', tbl => {
-    tbl.increments();
-    tbl.integer('zipCode', 5)
-      .notNullable()
-    tbl.integer('yearBuilt', 4)
+    })
+
+    .createTable('houses', tbl => {
+      tbl.increments();
+      tbl.integer('zipCode', 5)
         .notNullable()
-    tbl.integer('squareFootage', 5)
-    // float?
+      tbl.integer('yearBuilt', 4)
         .notNullable()
-    tbl.integer('bedrooms', 2)
+      tbl.integer('squareFootage', 5)
+      // float?
         .notNullable()
-    tbl.float('bathrooms', 2)
+      tbl.integer('bedrooms', 2)
         .notNullable()
-     
-})
-.createTable('fav', tbl => {
-    tbl.integer('userID', 3)
+      tbl.float('bathrooms', 2)
+        .notNullable()
+    })
+
+    .createTable('fav', tbl => {
+      tbl.integer('userID', 3)
         .unsigned()
         .notNullable()
         .references('id').inTable('users')
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
-    tbl.integer('houseID', 3)
+      tbl.integer('houseID', 3)
         .unsigned()
         .notNullable()
         .references('id').inTable('houses')
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
-    tbl.string('name', 256)
+      tbl.string('name', 256)
         .notNullable()
-    tbl.integer('interestLevel', 3)
-    tbl.primary(['userID', 'houseID'])
+      tbl.integer('interestLevel', 3)
+      tbl.primary(['userID', 'houseID'])
     })
-    
+
+    .createTable('prices', tbl => {
+      tbl.increments()
+      tbl.integer('houseID')
+        .unsigned()
+        .notNullable()
+        .references('id').inTable('houses')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      tbl.float('price')
+        .notNullable()
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema 
-  .dropTableIfExists('fav')
-  .dropTableIfExists('houses')
-  .dropTableIfExists('users')
+    .dropTableIfExists('prices')
+    .dropTableIfExists('fav')
+    .dropTableIfExists('houses')
+    .dropTableIfExists('users')
 };
