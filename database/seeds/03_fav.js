7672f146bdc99604ded21469024286d7e6d72fc4
Arvin Agas <arvagas@gmail.com>
2019-09-24 17:45:14 -0400

    edit 03_fav seed

--- database/seeds/03_fav.js
+++ database/seeds/03_fav.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
         {userID: 1, houseID: 3, name: 'Cool house', interestLevel: 75},
         {userID: 1, houseID: 2, name: 'Nice view from patio', interestLevel: 80},
         {userID: 2, houseID: 1, name: 'Great interior', interestLevel: 100},
-        {userID: 2, houseID: 3, name: 'Public transportation aAccessible', interestLevel: 91},
+        {userID: 2, houseID: 3, name: 'Public transportation accessible', interestLevel: 91},
         {userID: 2, houseID: 2, name: 'Great view', interestLevel: 88},
         {userID: 3, houseID: 2, name: 'Dog loves it', interestLevel: 73},
       ]);
