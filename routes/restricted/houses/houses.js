03213bb61130a4de11d5ea6fb9e04da400d1d20b
Arvin Agas <arvagas@gmail.com>
2019-09-24 12:37:10 -0400

    houses endpoint

--- routes/restricted/houses/houses.js
+++ routes/restricted/houses/houses.js
@@ -1,9 +1,49 @@
 const express = require('express');
+const router = express.Router();
+const Houses = require('./housesModel');
+//const mw = require('./housesMiddleware');
 
-const db = require('../../../api/db-config');
+router.get('/', (req, res) => {
+    console.log('invoked')
+    Houses.find()
+    .then(houses => {
+        console.log('get', houses)
+        res.status(200).json(houses)
+    })
+    .catch(err => res.status(400).json({ err: err.message }))
+});
 
-server.get('/', (req, res) => {
-    res.json({ test: "on the right track!"})
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    Houses.findById(id)
+    .then(houses => res.status(200).json(houses))
+    .catch(err => res.status(400).json({ err: err.message }))
+});
+
+router.post('/', (req, res) => {
+    const newHouse = req.body;
+
+    Houses.add(newHouse)
+        .then(house => res.status(201).json(newHouse))
+        .catch(err => res.status(400).json({ err: err.message }))
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Houses.update(id, changes)
+        .then(house => res.status(200).json(house))
+        .catch(err => res.status(400).json({ err: err.message }))
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Houses.remove(id)
+        .then(house => res.status(200).json(house))
+        .catch(err => res.status(400).json({ err: err.message }))
 });
 
 module.exports = router;
