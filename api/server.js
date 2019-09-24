160f98defefc7b7fa751c595cd250a2a6aa87757
Sean Doyle <sdoylelambda@gmail.com>
2019-09-24 14:03:16 -0400

    All mock data working plus login and auth.

--- api/server.js
+++ api/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const server = express();
 const aboutRoute = require('../routes/about/about');
 const favRoute = require('../routes/restricted/fav/fav');
+const authRouter = require('../auth/authRouter');
 const housesRoutes = require('../routes/restricted/houses/houses');
 
 server.use(express.json());
 server.use('/about', aboutRoute);
 server.use('/fav', favRoute);
 server.use('/houses', housesRoutes);
+server.use('/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.json({ test: 'its up'})
