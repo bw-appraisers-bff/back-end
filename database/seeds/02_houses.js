20fb7188ad01954511d1c240a61ede439650b63b
Arvin Agas <arvagas@gmail.com>
2019-09-26 15:03:04 -0400

    updated readme to reflect checks

--- database/seeds/02_houses.js
+++ database/seeds/02_houses.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('houses').insert([
-        {zipCode: 90210, yearBuilt: 1960, squareFootage: 1000, bedrooms: 10, bathrooms: 5.5},
+        {zipCode: 90210, yearBuilt: 1960, squareFootage: 1000, bedrooms: 8, bathrooms: 5.5},
         {zipCode: 10024, yearBuilt: 1975, squareFootage: 2500, bedrooms: 1, bathrooms: 0.5},
         {zipCode: 60007, yearBuilt: 1920, squareFootage: 5200, bedrooms: 3, bathrooms: 2},
         {zipCode: 29083, yearBuilt: 1987, squareFootage: 2800, bedrooms: 2, bathrooms: 2},
