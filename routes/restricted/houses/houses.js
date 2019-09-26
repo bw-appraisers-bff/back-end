318187177d8e7bde8dc0d6d191877ce8c6c97cc5
Arvin Agas <arvagas@gmail.com>
2019-09-26 15:51:42 -0400

    check to see if price is reasonable

--- routes/restricted/houses/houses.js
+++ routes/restricted/houses/houses.js
@@ -24,21 +24,24 @@ router.get('/:id', authMiddleware, mw.validateHouseId, (req, res) => {
 router.post('/', authMiddleware, mw.validateHouseObj, (req, res) => {
     const newHouse = req.body;
 
-    Houses.add(newHouse)
-        .then(house => {
-            axios.get(`https://appraisers-bff.herokuapp.com/?bedrooms=${house.bedrooms}&bathrooms=${house.bathrooms}&squarefeet=${house.squareFootage}&yearbuilt=${house.yearBuilt}&zipcode=11111`)
-                .then(price => {
-                    Prices.add(house.id, price.data)
-                        .then(() => {
-                            Houses.findByIdWithPrice(house.id)
-                                .then(hse => res.status(201).json(hse))
-                                .catch(err => res.status(500).json({ message: 'error retrieving house results with price' }))
-                        })
-                        .catch(err => res.status(500).json({ message: 'error inputting price' }))
-                })
-                .catch(err => res.status(500).json({ message: 'error calculating price' }))
+    axios.get(`https://appraisers-bff.herokuapp.com/?bedrooms=${newHouse.bedrooms}&bathrooms=${newHouse.bathrooms}&squarefeet=${newHouse.squareFootage}&yearbuilt=${newHouse.yearBuilt}`)
+        .then(price => {
+            if (price.data < 10000 || price.data > 25000000) res.status(400).json({ message: 'We are unable to appraise a house with the features you described. Please try with again with different attributes.' })
+            else {
+                Houses.add(newHouse)
+                    .then(house => {
+                        Prices.add(house.id, price.data)
+                            .then(() => {
+                                Houses.findByIdWithPrice(house.id)
+                                    .then(hse => res.status(201).json(hse))
+                                    .catch(err => res.status(500).json({ message: 'error retrieving house results with price' }))
+                            })
+                            .catch(err => res.status(500).json({ message: 'error inputting price' }))
+                    })
+                    .catch(err => res.status(500).json({ err: err.message }))
+            }
         })
-        .catch(err => res.status(500).json({ err: err.message }))
+        .catch(err => res.status(500).json({ message: 'error calculating price' }))
 });
 
 router.put('/:id', authMiddleware, mw.validateHouseId, mw.validateHouseObj, (req, res) => {
