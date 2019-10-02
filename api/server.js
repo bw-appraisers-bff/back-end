bb1b2709d5da466d8886b5d3d6378627951a6864
Arvin Agas <arvagas@gmail.com>
2019-10-02 10:50:05 -0400

    waking up DS model via / endpoint

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const cors = require('cors')
+const axios = require('axios')
 const aboutRoute = require('../routes/about/about');
 const favRoute = require('../routes/restricted/fav/fav');
 const authRouter = require('../auth/authRouter');
@@ -14,7 +15,10 @@ server.use('/houses', housesRoutes);
 server.use('/auth', authRouter);
 
 server.get('/', (req, res) => {
-    res.json({ test: 'its up'})
+    // let's wake up the DS model first
+    axios.get(`https://appraisers-bff.herokuapp.com/?&yearbuilt=1983&bedrooms=1&bathrooms=2&squarefeet=1800`)
+        .then(price => res.json({ apiTest: 'its up', dsTest: 'its up' }))
+        .catch(err => res.status(500).json({ message: 'error waking up the data science model' }))
 });
 
 module.exports = server;
